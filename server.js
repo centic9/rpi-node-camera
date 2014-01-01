b7d1832c50be8b4af0ae39fccc8a262fe3ca8fe7
Mikael Soderstrom <mikael.soderstrom@live.se>
2014-01-01 14:41:09 +0100

    Added a html file and socket

---
+++ server.js
@@ -0,0 +1,23 @@
+﻿var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server, { log: false });
+var fs = require('fs');
+
+var imagePath = '/home/pi/camera';
+
+app.set('view options', { layout: false });
+app.use(express.static(__dirname + '/public'));
+
+app.get('/camera.jpg', function (req, res) {
+    res.sendfile(imagePath + '/surveillance.jpeg');
+});
+
+io.sockets.on('connection', function (socket) {
+    fs.watch(imagePath, function () {
+        socket.emit('camera', { status: 'updated' });
+    });
+});
+
+server.listen(3000);
+console.log('Listening on port 3000');

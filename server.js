52f724da9408c363a39b44c3716dbe8096258c44
Mikael Soderstrom <mikael.soderstrom@live.se>
2014-01-01 14:50:12 +0100

    Added modified time

--- server.js
+++ server.js
@@ -5,17 +5,25 @@ var io = require('socket.io').listen(server, { log: false });
 var fs = require('fs');
 
 var imagePath = '/home/pi/camera';
+var imageFullPath = imagePath + '/surveillance.jpeg';
 
 app.set('view options', { layout: false });
 app.use(express.static(__dirname + '/public'));
 
 app.get('/camera.jpg', function (req, res) {
-    res.sendfile(imagePath + '/surveillance.jpeg');
+    res.sendfile(imageFullPath);
 });
 
 io.sockets.on('connection', function (socket) {
+    fs.stat(imageFullPath, function (err, stats) {
+        socket.emit('modified', { time: stats.mtime });
+    });
+
     fs.watch(imagePath, function () {
-        socket.emit('camera', { status: 'updated' });
+        fs.stat(imageFullPath, function (err, stats) {
+            socket.emit('camera', { status: 'updated' });
+            socket.emit('modified', { time: stats.mtime });
+        });
     });
 });
 

305e5ea5005ec4618c2c0f84213a8c2cff37a509
Mikael Soderstrom <mikael.soderstrom@live.se>
2014-01-04 17:26:23 +0100

    Moved client javascript to a separate file

---
+++ public/app.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    var socket = io.connect('http://raspbmc:3000');
+
+    function refreshImage() {
+        document.getElementById('image').src = 'camera.jpg?' + new Date().getTime();
+        document.getElementById('modified').innerHTML = 'Last modified: ' + new Date(data.time).toLocaleString();
+    }
+
+    socket.on('modified', function (data) {
+        refreshImage();
+    });
+
+    refreshImage();
+})();
